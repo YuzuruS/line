7d4b58f1d30f0b22aa170d32a0bc21ccfdbb4bdc
YuzuruS <navitima@gmail.com>
2016-04-14 14:21:22 +0900

    refactoring

--- index.php
+++ index.php
@@ -1,56 +1,68 @@
 <?php
-define('API_KEY', '');
-define('MODE', 'dialog');
-define('CHANNEL_ID', );
-define('CHANNEL_SECRET', '');
-define('MID', '');
+// 設定
+define('DOCOMO_API_KEY', '');
+define('DOCOMO_MODE', 'dialog');
+define('LINE_CHANNEL_ID', '');
+define('LINE_CHANNEL_SECRET', '');
+define('LINE_MID', '');
+
 require_once(__DIR__ . '/vendor/autoload.php');
 use jp3cki\docomoDialogue\Dialogue;
 use Symfony\Component\HttpFoundation\Request;
-error_log('aga');
+
 $app = new Silex\Application();
 $app->post('/callback', function (Request $request) use ($app) {
+    // リクエスト取得
+    $request = file_get_contents("php://input");
+    $json = json_decode($request);
+    $content = $json->result[0]->content;
+    $message = $content->text;
 
+    // コンテキストIDを取得
+    $from = $content->from;
+    $redis = new Redis();
+    $redis->connect("127.0.0.1",6379);
+    $context = $redis->get($from);
 
-$request = file_get_contents("php://input");
-$json = json_decode($request);
-$content = $json->result[0]->content;
-$message = $content->text;
+    // 送信パラメータの準備
+    $dialog = new Dialogue(DOCOMO_API_KEY);
+    $dialog->parameter->reset();
+    $dialog->parameter->utt = $message;
+    $dialog->parameter->context = $context;
+    $dialog->parameter->mode = DOCOMO_MODE;
 
-// コンテキストIDを取得
-$from = $content->from;
-$redis = new Redis();
-$redis->connect("127.0.0.1",6379);
-$context = $redis->get($from);
-$dialog = new Dialogue(API_KEY);
+    // 対話
+    $ret = $dialog->request();
+    // コンテキストIDを保存
+    $redis->set($from, $ret->context);
 
-// 送信パラメータの準備
-$dialog->parameter->reset();
-$dialog->parameter->utt = $message;
-$dialog->parameter->context = $context;
-$dialog->parameter->mode = MODE;
+    $response_format_text = [
+        'contentType' => 1,
+        "toType" => 1,
+        "text" => "$ret->utt"
+    ];
+    $post_data = [
+        "to" => [
+            $from
+        ],
+        "toChannel" => "1383378250", // 固定値
+        "eventType" => "138311608800106203", // 固定値
+        "content" => $response_format_text
+    ];
 
-// 対話
-$ret = $dialog->request();
-// コンテキストIDを保存
-$redis->set($from, $ret->context);
-
-$response_format_text = ['contentType'=>1, "toType"=>1, "text"=>"$ret->utt"];
-$post_data = ["to"=>[$from],"toChannel"=>"1383378250","eventType"=>"138311608800106203","content"=>$response_format_text];
-
-// LINEに送信
-$ch = curl_init("https://trialbot-api.line.me/v1/events");
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    'Content-Type: application/json; charser=UTF-8',
-    "X-Line-ChannelID: " . CHANNEL_ID,
-    "X-Line-ChannelSecret: " . CHANNEL_SECRET,
-    "X-Line-Trusted-User-With-ACL: " . MID,
-]);
-curl_exec($ch);
-curl_close($ch);
+    // LINEに送信
+    $ch = curl_init("https://trialbot-api.line.me/v1/events");
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json; charser=UTF-8',
+        "X-Line-ChannelID: " . LINE_CHANNEL_ID,
+        "X-Line-ChannelSecret: " . LINE_CHANNEL_SECRET,
+        "X-Line-Trusted-User-With-ACL: " . LINE_MID,
+    ]);
+    curl_exec($ch);
+    curl_close($ch);
 });
 $app->run();
